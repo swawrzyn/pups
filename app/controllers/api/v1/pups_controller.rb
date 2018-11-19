327dd9f3a369bbe9207ee9013d4ae396414e041f
Tommy Siu <tommysiu0@hotmail.com>
2018-11-19 22:05:07 +0800

    created UPDATE, CREATE and DELETE in Pups Controller

--- app/controllers/api/v1/pups_controller.rb
+++ app/controllers/api/v1/pups_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PupsController < Api::V1::BaseController
+
   def index
     @pups = Pup.all
   end
@@ -7,5 +8,37 @@ class Api::V1::PupsController < Api::V1::BaseController
     @pup = Pup.find(params[:id])
   end
 
+  def update
+    @pup = Pup.find(params[:id])
+    if (@pup.update pup_params)
+      render :show
+    else
+      render_error
+    end
+  end
 
+  def create
+    @pup = Pup.new(pup_params)
+    if (@pup.save)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @pup = Pup.find(params[:id])
+    @pup.destroy
+  end
+
+
+  private
+    def pup_params
+      params.require(:pup).permit(:name, :location, :description, :image, :price)
+    end
+
+    def render_error
+      render json: { error: @pup.errors.full_messages },
+      status: :unprocessable_entity
+    end
 end
