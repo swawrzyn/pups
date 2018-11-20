2584a33eae77d7fc8c0516563dcc03559ba4ce0a
Tommy Siu <tommysiu0@hotmail.com>
2018-11-20 14:39:28 +0800

    :id added to jbuilder

---
+++ app/views/api/v1/bookings/new.json.jbuilder
@@ -0,0 +1,4 @@
+new.json.jbuilder
+# GET Pup information
+# POST BOOKING INFORMATION
+# POST NEW BOOKING
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-  namespace :v1 do
-    resources :pups
-  end
-end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :pups
+      resources :pups, only: [ :index, :show, :update, :create, :destroy ]
+      resources :bookings, only: [ :new, :show, :update, :create ]
     end
   end
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
