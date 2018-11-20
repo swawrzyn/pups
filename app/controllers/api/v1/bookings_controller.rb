cdd9570c1d1f1823b22181621db3595e90a02422
Tommy Siu <tommysiu0@hotmail.com>
2018-11-20 16:13:50 +0800

    removed accepted from bookings controller'

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -30,7 +30,7 @@ class Api::V1::BookingsController < Api::V1::BaseController
   private
 
   def booking_params
-    params.require(:booking).permit(:pup_id, :user_id, :time_start, :time_end, :accepted)
+    params.require(:booking).permit(:pup_id, :user_id, :time_start, :time_end)
   end
 
   def render_error
