95294cb9b41ace55b1cd30510af365ce9e9daada
Tommy Siu <tommysiu0@hotmail.com>
2018-11-20 11:39:47 +0800

    finished off seed database for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,6 @@ end
 User.all.each do |user|
   Pup.create!(user: user, name: Faker::Company.name,location: Faker::Address.street_address,description: Faker::GameOfThrones.quote, price: Random.new.rand(100..1000),image: "https://picsum.photos/200/300/?random")
 end
+
+Booking.create(pup: Pup.first, user: User.first, time_start: "2018-11-17", time_end: "2018-11-18", accepted: true)
+Booking.first.reviews.create(content: "this space was great", rating: 4, user: User.first)
