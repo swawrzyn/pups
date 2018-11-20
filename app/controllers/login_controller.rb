d9e7a6d65b1d37a1dc6cae000a9b036d062215b0
Tommy Siu <tommysiu0@hotmail.com>
2018-11-20 15:57:33 +0800

    completed wechat login setup on rails

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,10 +3,23 @@ class LoginController < ApplicationController
 
   def wechat_params
     {
-      appid: appId  ENV[],
+      appid: ENV['WECHAT_APP_ID'],
       secret: appSecret,
       js_code: params[:code],
       grant_type: "authorization_code"
     }
   end
+
+  def wechat_user
+    @wechat_response ||= RestClient.post(URL, wechat_params)
+    @wechat_user ||= JSON.parse(@wechat_response.body)
+  end
+
+  def login
+    @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
+    render json: {
+      userId: @user.id
+    }
+  end
+
 end
