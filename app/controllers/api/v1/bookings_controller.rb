544b98d8c1885fdb91adbe7411e864eefc047bfc
Tommy Siu <tommysiu0@hotmail.com>
2018-11-20 15:38:58 +0800

    completed bookings controller,routes+show.jbuilder

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -30,7 +30,7 @@ class Api::V1::BookingsController < Api::V1::BaseController
   private
 
   def booking_params
-    params.require(:booking).permit(:pup_id, :user_id, :time_start, :time_end)
+    params.require(:booking).permit(:pup_id, :user_id, :time_start, :time_end, :accepted)
   end
 
   def render_error
