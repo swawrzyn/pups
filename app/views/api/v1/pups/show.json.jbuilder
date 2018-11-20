7c618f8f673b67fed637b5c0760b55aed4723946
Tommy Siu <tommysiu0@hotmail.com>
2018-11-20 11:48:10 +0800

    fixed seed + added avg_rating to show.jsonbuilder

--- app/views/api/v1/pups/show.json.jbuilder
+++ app/views/api/v1/pups/show.json.jbuilder
@@ -1,4 +1,4 @@
-json.extract! @pup, :name, :location, :description, :image, :price
+json.extract! @pup, :name, :location, :description, :image, :price, :avg_rating
   json.reviews @pup.reviews do |review| #is pup.bookings.reviews or pup.reviews?
     json.extract! review, :content, :rating
     json.date review.created_at.strftime('%m/%d/%y')
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 require 'faker'
 
+User.destroy.all
+
 10.times do
   User.create!(open_id: Faker::IDNumber.valid)
 end
@@ -16,5 +18,5 @@ User.all.each do |user|
   Pup.create!(user: user, name: Faker::Company.name,location: Faker::Address.street_address,description: Faker::GameOfThrones.quote, price: Random.new.rand(100..1000),image: "https://picsum.photos/200/300/?random")
 end
 
-Booking.create(pup: Pup.first, user: User.first, time_start: "2018-11-17", time_end: "2018-11-18", accepted: true)
+Booking.create(pup: Pup.first, user: User.last, time_start: "2018-11-17", time_end: "2018-11-18", accepted: true)
 Booking.first.reviews.create(content: "this space was great", rating: 4, user: User.first)
