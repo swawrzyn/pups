fc7e479567472e2fcf0800023da918b93a73c912
Tommy Siu <tommysiu0@hotmail.com>
2018-11-20 21:24:46 +0800

    updated reviews controller and need to review show users JSON

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :pups, dependent: :destroy
   has_many :bookings
+  # has_many :reviews, ->(object) { where "review.user_id != #{object.id}" }, through: :bookings
   has_many :reviews, through: :bookings
   validates :open_id, uniqueness: true, presence: true
 end
